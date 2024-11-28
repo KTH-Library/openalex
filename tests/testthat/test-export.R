981bda6ab643b83236de51e83662eb1ed7027fe9
fileunderwater <file_under_water@yahoo.se>
2024-11-28 17:30:36 +0100

    fix typo

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -5,7 +5,7 @@ test_that("export works", {
   my_filter <- paste0(collapse = ",", c(
     "authorships.institutions.lineage:i86987016", ## KTH
     "authorships.institutions.lineage:i4210161097", ## Bolin Center (some of these might be KTH pubs!)
-    "authorships.institutions.lineage:i119971240", ## NORDITA (some of these, too!) 
+    "authorships.institutions.lineage:i119971240", ## NORDITA (some of these, too!)
     "authorships.institutions.lineage:i4210147696", ## THS Tekniska Högskolans Studentkår
     "type:types/article",
     "primary_location.source.type:source-types/journal|source-types/conference",
@@ -17,7 +17,7 @@ test_that("export works", {
   # for retroactive import - exclude centres
 
   my_csv <- openalex_works_export(q = my_filter, fmt = "csv")
-  my_csv_string <- openalex_works_export(q = my_filter, fmt = "csv", raw_string = TRUE)  
+  my_csv_string <- openalex_works_export(q = my_filter, fmt = "csv", raw_string = TRUE)
   my_wos <- openalex_works_export(q = my_filter, fmt = "wos-plaintext")
   my_wos_string <- openalex_works_export(q = my_filter, fmt = "wos-plaintext", raw_string = TRUE)
 
@@ -30,19 +30,19 @@ test_that("export for diva in wos-plain text format works", {
   skip()
 
   my_filter <- paste0(collapse = ",", c(
-    sprintf("publication_year:%s", 2024),    
+    sprintf("publication_year:%s", 2024),
     sprintf("authorships.author.id:%s", "a5045975901") #,
   #   sprintf("raw_affiliation_strings.search:%s", openalex_kth_rawaff_query()),
   #   "authorships.institutions.lineage:!i86987016", ## KTH
   #   "authorships.institutions.lineage:!i4210161097", ## Bolin Center (some of these might be KTH pubs!)
-  #   "authorships.institutions.lineage:!i119971240", ## NORDITA (some of these, too!) 
-  #   "authorships.institutions.lineage:!i4210147696" ## THS Tekniska Högskolans Studentkår    
+  #   "authorships.institutions.lineage:!i119971240", ## NORDITA (some of these, too!)
+  #   "authorships.institutions.lineage:!i4210147696" ## THS Tekniska Högskolans Studentkår
   ))
 
   gm <- openalex_works_export(q = my_filter, fmt = "wos-plaintext")
 
   cat(gm)
-  
+
   is_valid <- (regmatches(gm, gregexpr("Maguire", gm)) |> unlist()) |> length() > 10
   expect_true(is_valid)
 })
@@ -50,12 +50,12 @@ test_that("export for diva in wos-plain text format works", {
 test_that("export of rawff query for 2024 in wos-plain diva text format works", {
 
   my_filter <- paste0(collapse = ",", c(
-    sprintf("publication_year:%s", 2024),    
+    sprintf("publication_year:%s", 2024),
     sprintf("raw_affiliation_strings.search:%s", openalex_kth_rawaff_query()),
     "authorships.institutions.lineage:!i86987016", ## KTH
     "authorships.institutions.lineage:!i4210161097", ## Bolin Center (some of these might be KTH pubs!)
-    "authorships.institutions.lineage:!i119971240", ## NORDITA (some of these, too!) 
-    "authorships.institutions.lineage:!i4210147696" ## THS Tekniska Högskolans Studentkår    
+    "authorships.institutions.lineage:!i119971240", ## NORDITA (some of these, too!)
+    "authorships.institutions.lineage:!i4210147696" ## THS Tekniska Högskolans Studentkår
   ))
 
   extras <- openalex_works_export(q = my_filter, fmt = "wos-plaintext")
@@ -68,7 +68,7 @@ test_that("export of rawff query for 2023 in csv format works", {
   skip()
 
   my_filter <- paste0(collapse = ",", c(
-    sprintf("publication_year:%s", 2023),    
+    sprintf("publication_year:%s", 2023),
     sprintf("raw_affiliation_strings.search:%s", openalex_kth_rawaff_query())
   ))
 
@@ -87,7 +87,7 @@ test_that("export of rawff query for 2023 in csv format works", {
   skip()
 
   my_filter <- paste0(collapse = ",", c(
-    sprintf("publication_year:%s", 2023),    
+    sprintf("publication_year:%s", 2023),
     sprintf("raw_affiliation_strings.search:%s", openalex_kth_rawaff_query())
   ))
 
@@ -99,8 +99,8 @@ test_that("export of rawff query for 2023 in csv format works", {
 
   expect_true(is_valid)
 
-
 }
+)
 
 
 
