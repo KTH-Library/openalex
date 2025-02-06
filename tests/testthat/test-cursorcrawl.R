1116ba1fe8bd14368fbf8a83b53a8a532e2b2153
Markus Skyttner <mskyttner@pm.me>
2025-02-06 16:25:03 +0100

    attempt to use jqr instead of syscall when filtering with jq

--- tests/testthat/test-cursorcrawl.R
+++ tests/testthat/test-cursorcrawl.R
@@ -6,10 +6,16 @@ test_that("cursor based paging for works works", {
 
   cc <- 
     works_filter |> 
-    openalex_works_cursorcrawl(n_max_pages = 10)
+    openalex_works_cursorcrawl(n_max_pages = 2)
 
   mydir <- unique(dirname(cc))
-  is_valid <- all(cc %in% dir(mydir, full.names = TRUE))
+  
+  is_valid <- 
+    all(cc %in% dir(mydir, full.names = TRUE)) &
+    jsonlite::stream_in(file(cc[1])) |> nrow()
+
+  # TODO: Hmmm, what is a$abstract_inverted_index_v3?
+  # a <- jsonlite::stream_in(file(cc[1])) |> as_tibble()
 
   expect_true(is_valid)
 
